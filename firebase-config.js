fbeba85552eadbd4d9742af97d3e0293da82c882
Padmavathi-Mallavarapu <mallavarapupadmavathi1463@gmail.com>
2026-01-07 22:10:04 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,19 @@
 // firebase-config.js
 // Replace these values with YOUR actual Firebase project credentials
-const firebaseConfig = {
-  apiKey: "AIzaSyCvuKqgsgtIAPzh5U569-FksQdMaHjcyjE",
-  authDomain: "student-fee-reminder.firebaseapp.com",
-  projectId: "student-fee-reminder",
-  storageBucket: "student-fee-reminder.firebasestorage.app",
-  messagingSenderId: "652699992618",
-  appId: "1:652699992618:web:35edac6c9581c023d37953"
-};
+ const firebaseConfig = {
+    apiKey: "AIzaSyCvuKqgsgtIAPzh5U569-FksQdMaHjcyjE",
+    authDomain: "student-fee-reminder.firebaseapp.com",
+    projectId: "student-fee-reminder",
+    storageBucket: "student-fee-reminder.firebasestorage.app",
+    messagingSenderId: "652699992618",
+    appId: "1:652699992618:web:35edac6c9581c023d37953",
+    measurementId: "G-KMK6DPJ20S"
+  };
+
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 console.log("Firebase initialized successfully!");
+
 
